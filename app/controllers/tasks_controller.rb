594e274fb940c78565272f678c83d0745f7689bc
yumi yonemori <yumiyonemori@gmail.com>
2023-02-20 23:15:35 +0900

    タスクの新規作成画面、編集画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,31 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: '保存しました'
+    else
+      render :new
+    end
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: '保存しました'
+    else
+      render :edit
+    end
   end
 
   private
