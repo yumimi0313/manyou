5db3511a59e7f06acf5fe02c07395af3152dfb8a
yumi yonemori <yumiyonemori@gmail.com>
2023-02-23 13:40:04 +0900

    終了期限カラム追加、降順にする

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,11 @@ class TasksController < ApplicationController
   before_action :set_task, only:[:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.order(created_at: :desc)
+    if params[:sort_expired]
+      @tasks = Task.all.order(due_date: :desc)
+    else
+      @tasks = Task.all.order(created_at: :desc)
+    end
   end
 
   def new
